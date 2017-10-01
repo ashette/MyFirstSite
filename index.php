aa69404680f9d5a2477e2af0d096075db42447a2
ASH <anastasiansh@gmail.com>
2017-10-01 18:18:20 +0400

    вставила фотку, подогнала их по размерам

--- index.php
+++ index.php
@@ -1,45 +1,49 @@
 ﻿<!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8"> 
-     <title>сайт</title>
-<link rel="stylesheet" type="text/css" href="style.css">
+    <head>
+        <meta charset="UTF-8"> 
+        <title>сайт</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
 
-</head>
-<body>
-<div id="container">
-	<div id="header">
-	<h2>ANASTASIA SHULGA</h2>    
-        <div id="a">
-        <a> MAKE-UP ARTIST</a>
+    </head>
+    <body>
+        <div id="container">
+            <div id="header">
+                <div id="logo">
+                    <h2><a href="#">ANASTASIA SHULGA</a></h2>    
+                </div> 
+                <div id="a">
+                    <a> MAKE-UP ARTIST</a>
+                </div>
+            </div>       
+
+            <div id="navigation">
+                <table id="menu">
+                    <tr>
+                        <td><a href="index.php"  title="">Главная</a></td>
+                        <td><a href="portfolio.php"  title="">Портфолио</a></td>
+                        <td><a href="about.php"  title="">Обо мне</a></td>
+                        <td><a href="book.php"  title="">Записаться</a></td>
+                        <td><a href="contacts.php"  title="">Контакты</a></td>
+                    </tr>
+                </table>
+            </div>
+
+
+
+            <div id="content">   
+                <div id="crop">
+                    <img style="width: 100%; height: 800px" src="images/1.jpg"  alt="">
+                </div>
+            </div>
+
+            <div id="clear">
+
+            </div>
+
+            <div id="footer">
+                <h2>footer </h2>
+            </div>
         </div>
-	</div>       
-    
-	<div id="navigation">
-	<table id="menu">
-		<tr>
-			<td><a href="#"  title="">Главная</a></td>
-			<td><a href="#"  title="">Портфолио</a></td>
-			<td><a href="#"  title="">Обо мне</a></td>
-			<td><a href="#"  title="">Записаться</a></td>
-			<td><a href="#"  title="">Контакты</a></td>
-		</tr>
-		</table>
-	</div>
-		 
-
-		 
-	<div id="content">
-	<h2>контент</h2>
-	</div>
-		 
-	<div id="clear">
-		 
-	</div>
-							   
-	<div id="footer">
-	<h2>footer </h2>
-	</div>
-</div>
-</body>
+    </body>
 </html>
